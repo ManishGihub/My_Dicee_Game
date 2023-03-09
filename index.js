e8b505b0dc1d01d231e8f4ece5bd544864568753
ManishGihub <102681545+ManishGihub@users.noreply.github.com>
2023-03-09 16:22:11 +0530

    Done

---
+++ index.js
@@ -0,0 +1,22 @@
+// for first image
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+document
+  .querySelectorAll("img")[0]
+  .setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+// for second image
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+document
+  .querySelectorAll("img")[1]
+  .setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
+else if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!🚩";
+}
+else{
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+}
